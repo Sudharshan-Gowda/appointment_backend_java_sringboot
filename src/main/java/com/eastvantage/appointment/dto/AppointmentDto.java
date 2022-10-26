02c4f2f3c48d7e04053efa47f5cf7d52653a8f31
Sudharshan B S <sudharshan.bs@testyantra.in>
2022-10-26 13:11:26 +0530

    validations updated

--- src/main/java/com/eastvantage/appointment/dto/AppointmentDto.java
+++ src/main/java/com/eastvantage/appointment/dto/AppointmentDto.java
@@ -2,6 +2,7 @@ package com.eastvantage.appointment.dto;
 
 import java.time.LocalDateTime;
 
+import javax.validation.constraints.FutureOrPresent;
 import javax.validation.constraints.Pattern;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -15,8 +16,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class AppointmentDto {
 
+	@FutureOrPresent(message = "Provide A Valid Future Date")
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM-dd-yyyy HH:mm:ss")
 	private LocalDateTime appointmentDateTime;
+
 	private String appointmentName;
 
 	@Pattern(regexp = "(?:[01]\\d|2[0123]):(?:[012345]\\d):(?:[012345]\\d)", message = "Provide duration in the format of HH:MM:SS")
